fff1b42acbeea453215f700491541fb45ef716fd
iKevinDeveloper <79568841+iKevinDeveloper@users.noreply.github.com>
2021-05-16 11:49:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+const https = require('https');
+const fs = require('fs');
 const config = require('config');
 const util = require('util');
 const app = require('./app');
+
+https.createServer({
+   cert: fs.readFileSync('certs/cert.pem'),
+   key: fs.readFileSync('certs/privkey.pem')
+ }, app).listen(process.env.PORT_HTTPS || config.port_https || 443, function(){
+        util.log('Server started: https://%s:%s/', this.address().address, this.address().port_https);
+});
 
 module.exports = app.listen(process.env.PORT || config.port || 80, function() {
 	util.log('Server started: http://%s:%s/', this.address().address, this.address().port);
